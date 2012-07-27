7094392722cc2521778364ae881a91f47b91d6b5
Timo Josten <tj@mkswap.net>
2012-07-27 12:52:51 +0300

    Update blog/urls.py
    TinyMCE is no longer required as separate python package as we're now using plain javascript stuff.

--- blog/urls.py
+++ blog/urls.py
@@ -8,9 +8,6 @@ urlpatterns = patterns('',
     # The entry_view, displaying a single Entry with comments etc.
     url(r'^(?P<url_slug>([-\w]+))/$', 'blog.views.entry_view', name='blog-entry'),
     url(r'^(?P<url_slug>([-\w]+))/(?P<page_num>(\d+))/$', 'blog.views.entry_view', name='blog-entry'),
-
-    # TinyMce URLs
-    (r'^tinymce/', include('tinymce.urls')),
 )
 
 
